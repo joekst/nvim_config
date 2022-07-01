cbe1b7ebd43b49faa8ee9b18a75526dd8ec9fa4e
Jonas Ekstrand <joekst@gmail.com>
2022-07-01 07:32:52 +0200

    Fixed so neoclip works with telescope

--- nvim/lua/jed/treesitter.lua
+++ nvim/lua/jed/treesitter.lua
@@ -23,8 +23,8 @@ require('nvim-treesitter.configs').setup({
   },
 })
 
-vim.api.nvim_exec([[
-  set foldmethod=expr
-  set foldexpr=nvim_treesitter#foldexpr()
-  ]], true)
+-- vim.api.nvim_exec([[
+--   set foldmethod=expr
+--   set foldexpr=nvim_treesitter#foldexpr()
+--   ]], true)
 
